748c4d542b71277b36418fbbf0edbcb9806196c0
KashifNadeemKayani <KAYANIKASHIF970@GMAIL.COM>
2024-09-19 19:41:51 +0500

    add

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/mart/',
   server: {
+
     host: true, // Listen on all network interfaces
   },
 })
