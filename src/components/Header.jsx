23db20059993ac522a899f4f327ac565a027f555
TheReliableMart <thereliablemart@gmail.com>
2024-09-26 10:10:15 +0500

    test check

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'; // Assuming you're using CSS Modules
 function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.brandName}>The Reliable Mart</h1>
+      <h1 className={styles.brandName}> Reliable Mart</h1>
       <p className={styles.description}>Where the Quality Matters</p>
     </header>
   );
