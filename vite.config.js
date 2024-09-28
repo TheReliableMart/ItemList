a03ae8c732521ffc965d0ec55ef3b2b3e3bb9c4e
TheReliableMart <thereliablemart@gmail.com>
2024-09-28 18:37:58 +0500

    changed name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/RateList/',
+   base: '/ItemList/',
   server: {
 
     host: true, // Listen on all network interfaces
