78825f2f2738a2dc8449f435aa74c2f46df388ff
Kashif <kayanikashif970@gmail.com>
2025-08-01 15:21:54 +0500

    footer added (name)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,17 +6,19 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <div className={styles.container}>
-    <footer className={styles.footer}>
-      <p>
-      {/* <a href="https://chat.whatsapp.com/C4fQhFyWbTC0vQkcJqeA7V" className={`text-white mx-3 ${styles.anker}`} target="_blank" rel="noopener noreferrer" >
+      <footer className={styles.footer}>
+        <p>
+          {/* <a href="https://chat.whatsapp.com/C4fQhFyWbTC0vQkcJqeA7V" className={`text-white mx-3 ${styles.anker}`} target="_blank" rel="noopener noreferrer" >
       <FontAwesomeIcon icon={faWhatsapp} size="2x"/></a> */}
-      © 2024 The Reliable Mart. All rights reserved.
-      </p>
-     
-    </footer>
+          © 2024 The Reliable Mart. All rights reserved.
+        </p>
+        <p>
+          A website by
+          <a href="https://kashifnadeemkayani.github.io/Portfolio/"> Kashif Nadeem Kayani</a>
+        </p>
+      </footer>
     </div>
   );
 }
 
 export default Footer;
-  
